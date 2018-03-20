6be8de40ba975a517ae01b3e34b1a181eb8171d0
Jeremiah Andrews <jlandrews@berkeley.edu>
2018-03-19 17:42:57 -0700

    Added throttling to enforce hashing over time

--- contracts/HashMoney.sol
+++ contracts/HashMoney.sol
@@ -7,20 +7,35 @@ contract HashMoney is EIP20 {
   uint8 public decimals = 18;
   string public symbol = "HMX";
 
+  uint public MIN_BLOCK_DELAY = 1000;
+
   function HashMoney() EIP20(0,name,decimals,symbol) public {
 
   }
 
-  mapping(address => uint) public pastClaims;
-
-  function claim(uint nonce) public {
-    require(nonce > pastClaims[msg.sender]);
-    bytes32 h = keccak256(msg.sender, nonce);
-    balances[msg.sender] += (uint(1) << countLeadingZeroes(h));
-    pastClaims[msg.sender] = nonce;
+  struct Claim {
+    uint blocknum;
+    uint numZeroes;
+    bytes32 blockhash;
   }
 
-  function countLeadingZeroes(bytes32 x) public returns (uint) {
+
+  mapping(address => Claim) public pastClaims;
+
+  function claim(uint nonce) public {
+    Claim memory cur = pastClaims[msg.sender];
+    uint LZ;
+    if (cur.blocknum == 0) {
+      LZ = countLeadingZeroes(keccak256(msg.sender, nonce));
+    } else {
+      require(block.number > cur.blocknum + MIN_BLOCK_DELAY);
+      LZ = countLeadingZeroes(keccak256(msg.sender, cur.blockhash, nonce));
+      balances[msg.sender] += (uint(1)) << ((LZ + cur.numZeroes)/2);
+    }
+    pastClaims[msg.sender] = Claim(block.number, LZ, block.blockhash(block.number));
+  }
+
+  function countLeadingZeroes(bytes32 x) public pure returns (uint) {
     for (uint i = 0; i < 256; i++) {
       if (uint(x[i/8])&(uint(1)<<(7-(i%8))) != 0) {
         return i;
